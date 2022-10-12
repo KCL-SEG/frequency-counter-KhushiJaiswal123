6ddf59de883063dde6be0ed65fa64f18148b044a
Khushi Jaiswal <14khushijais@gmail.com>
2022-10-12 15:39:06 +0100

    complete Frequency counter excercise

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,13 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+
+    #convert all items to strings
+    itemsAsStrings = []
+    for x in items:
+         itemsAsStrings.append (str(x))
+
+    #count occurances of each string
+    from collections import Counter
+    frequencies = Counter (itemsAsStrings)
     return frequencies
